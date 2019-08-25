65eff86161f5a343066d9b1da5b1288c69cc7375
Ben Clark <bclark@avalancheassociation.ca>
2019-08-25 12:16:36 -0700

    Got the database to connect, yay!

--- src/server.js
+++ src/server.js
@@ -1,17 +1,33 @@
-// var path = require("path");
+var path = require("path");
 var sqlite3 = require("sqlite3");
 var express = require("express");
 
 const app = express();
 
 const start = async () => {
-  const DB_PATH = "../database/db.sqlite";
+  const DB_PATH = path.join(__dirname, "database/db.sqlite");
 
   try {
-    // var myDB = new sqlite3.Database(DB_PATH);
-    app.listen(3001, () => {
-      console.log(`REST API on http://localhost:3001/api`);
+    let dbConnected = true;
+
+    console.log(`\nAttempting to connect to database at: ${DB_PATH} ... ... `);
+
+    var wfdb = new sqlite3.Database(DB_PATH, err => {
+      if (err) {
+        console.error("\x1b[31m", `\n Couldn't connect to the database :'( `);
+        console.error(err);
+        console.log("\n");
+        process.exit(1);
+      } else {
+        console.log("\nDatabase connected!");
+      }
     });
+
+    if (dbConnected) {
+      app.listen(3001, () => {
+        console.log(`\nREST API on http://localhost:3001/api \n `);
+      });
+    }
   } catch (e) {
     console.error(e);
   }
